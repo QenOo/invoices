5c0b8c316abacb600c8d827a7796faa40d5b75ea
QeNo <moodiiy.98@gmail.com>
2021-09-15 16:25:55 +0200

    add laravel lang and fix google fonts

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -18,4 +18,7 @@
 <!---Skinmodes css-->
 <link href="{{URL::asset('assets/css-rtl/skin-modes.css')}}" rel="stylesheet">
 
-
+{{--Font Arbic--}}
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
