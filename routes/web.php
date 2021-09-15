a5af9a51225b09c56612fac278a13ac762b212d4
QeNo <moodiiy.98@gmail.com>
2021-09-15 14:02:14 +0200

    add auth

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 
 Route::get('/{page}', [AdminController::class, 'index']);
